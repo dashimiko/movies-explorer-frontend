0b3eef9806dfa3c04f5b64239313407d8783752a
Daria Mikolaichuk <dashamikolaichuk@gmail.com>
2022-09-15 15:43:45 +0400

    fix styles

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -10,7 +10,7 @@ function Header({ onBurgerClick, isBurgerOpen }) {
     <div className={`header header__section_${location.pathname === '/' ? 'pink' : 'white'}`}>
       <div className="header__section section">
       <Link to='/' className='header__link button'>
-        <img src={logo} alt="Лого проекта" />
+        <img className ="header__logo" src={logo} alt="Лого проекта" />
       </Link>
       <Navigation onBurgerClick={onBurgerClick} isBurgerOpen={isBurgerOpen}/>
       </div>
