0e88eedd291c2510cfa47ff42faec8ea42c8158f
Daria Mikolaichuk <dashamikolaichuk@gmail.com>
2022-10-09 21:56:34 +0400

    fix profile validation

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -18,6 +18,7 @@ function Profile({signOut, handleSubmitUserInfo}) {
     },
     handleSubmit,
     reset,
+    setValue,
   } = useForm({
     mode: "onChange",
     defaultValues: {
@@ -28,10 +29,12 @@ function Profile({signOut, handleSubmitUserInfo}) {
 
   function handleNameChange(event) {
 		setName(event.target.value);
+    setValue('name', event.target.value);
 	};
 
 	function handleEmailChange(event) {
 		setEmail(event.target.value);
+    setValue('email', event.target.value);
 	};
 
   function handleUserInfo() {
@@ -43,12 +46,14 @@ function Profile({signOut, handleSubmitUserInfo}) {
 	useEffect(() => {
 		setName(currentUser.name);
 		setEmail(currentUser.email);
+    setValue('name', currentUser.name);
+    setValue('email', currentUser.email)
 	}, [currentUser]);
 
   useEffect(() => {
     if (name === currentUser.name && email === currentUser.email) {
 			setIsDisabledButton(true);
-		} else if (isValid || (name === currentUser.name || email !== currentUser.email) || (name !== currentUser.name || email === currentUser.email)) {
+		} else if (isValid) {
 			setIsDisabledButton(false);
 		} else if (!isValid) {
 			setIsDisabledButton(true);
@@ -62,9 +67,9 @@ function Profile({signOut, handleSubmitUserInfo}) {
       <form className="profile__form" noValidate onSubmit={handleSubmit(handleUserInfo)}>
         <div className="profile__form-box">
         <label className="profile__info">
-        {errors?.userName && <span className="name-error profile__error error">{errors?.userName?.message || "Что-то пошло не так..."}</span>}
+        {errors?.name && <span className="name-error profile__error error">{errors?.name?.message || "Что-то пошло не так..."}</span>}
           <span className="profile__value profile__value_name">Имя</span>
-          <input {...register("userName",
+          <input {...register("name",
           {required: "Поле обязательно к заполнению",
           minLength: {
             value: 2,
@@ -81,11 +86,13 @@ function Profile({signOut, handleSubmitUserInfo}) {
           onChange: (e) => handleNameChange(e),
           })}
           value={name || ""}
-        type="text" className="profile__input profile__input_name" placeholder={name || ""}/>
+          type="text"
+          className="profile__input profile__input_name"
+          placeholder={name || ""}/>
         </label>
         <label className="profile__info">
           <span className="profile__value">E-mail</span>
-          <input {...register("userEmail",
+          <input {...register("email",
           {required: "Поле обязательно к заполнению",
           minLength: {
             value: 2,
@@ -102,8 +109,13 @@ function Profile({signOut, handleSubmitUserInfo}) {
           onChange: (e) => handleEmailChange(e),
         })}
         value={email || ""}
-        type="email" className = "profile__input profile__input_email" placeholder={email || ""} minLength="2" maxLength="30" required/>
-          {errors?.userEmail && <span className="email-error profile__error error">{errors?.userEmail?.message || "Что-то пошло не так..."}</span>}
+        type="email"
+        className = "profile__input profile__input_email"
+        placeholder={email || ""}
+        minLength="2"
+        maxLength="30"
+        required/>
+          {errors?.email && <span className="email-error profile__error error">{errors?.email?.message || "Что-то пошло не так..."}</span>}
         </label>
         </div>
         <div className="profile__button-box">
